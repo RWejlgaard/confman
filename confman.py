e99dc0486da4c7a156b939cbc06aa949b58a4b97
RWejlgaard <wejlgaard@live.dk>
2017-10-22 23:08:09 +0200

    minor changes

--- confman.py
+++ confman.py
@@ -1,13 +1,32 @@
 import os
 import getpass
+import subprocess
 from crontab import CronTab
 
 def cronSetup():
-    localUser = getpass.getuser()
+    local_user = getpass.getuser()
 
-    cronFile = CronTab(user=localUser)
+    cron_file = CronTab(local_user)
+   
+def readConfig():
+    pass
+
+def cloneRepo(url, branch, path):
+    # TODO replace this with the pygit library thing
+    subprocess.call("git clone -b {branch} {url} {path}".format(**vars()), stdout=False)
+  
+def YNPrompt(question):
+    yes = ["yes", "ye", "y", "yup", "yeah"]
+    no = ["no", "n", "nope", "nah"]
     
-def readConfig()
+    promt = question + " [Y/n] "
 
-
+    while True:
+        answer = (input(promt)).lower()
+        if answer in yes:
+            return True
+        elif answer in no:
+            return False
+        else:
+            print("Please answer yes or no")
 
--- install.py
+++ install.py
@@ -1,7 +1,8 @@
-import os
+# import os
 import sys
-import git
-import getpass
+# import git
+# import getpass
+import confman
 import subprocess
 from crontab import CronTab
 
@@ -13,11 +14,18 @@ def install():
 
     print("Repo URL = " + gitURL)
     print("Branch = " + gitBranch)
-    
+   
     if not YNPrompt("Is the following correct?"):
         exit()
-
     
+    cloneRepo(gitURL, gitBranch, "/tmp/confman/dotfile_repo")
+    
+
+
+def cloneRepo(url, branch, path):
+    # TODO replace this with the pygit library thing
+    subprocess.call("git clone -b {branch} {url} {path}".format(**vars()), stdout=False)
+     
 
 
 def showHelp():
@@ -27,8 +35,8 @@ def showHelp():
     return exit()
 
 def YNPrompt(question):
-    yes = ["yes", "ye", "y"]
-    no = ["no", "n"]
+    yes = ["yes", "ye", "y", "yup", "yeah"]
+    no = ["no", "n", "nope", "nah"]
     
     promt = question + " [Y/n] "
 
